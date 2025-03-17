be00963cba006682f889ca9a2ec3b162244f610e
waghmares1172 <waghmares1172@gmail>
2025-03-17 13:48:40 +0530

    added subject

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-def function(id,marks,name,subject):
-    print(f"my name and id is {name} {id} and i got {marks} marks in {subject} subject")
-obj=function(101,100,"swamini","mathematics")
+def function(id,marks,name,subject1,subject2):
+    print(f"my name and id is {name} {id} and i got {marks} marks in {subject1} {subject2} subject")
+obj=function(101,100,"swamini","mathematics","science")
